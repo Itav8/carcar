9da53d26d60269ca0f0a269b07a3b08c2588155a
Jonathan <jonathan.r.blazer@gmail.com>
2023-04-24 19:16:35 -0400

    Service Models work

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class AutomobileVO(models.Model):
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+    model = models.CharField(max_length=50)
+
+    def get_api_url(self):
+        return reverse("api_automobile", kwargs={"vin": self.vin})
+
+    def __str__(self):
+        return f'{self.model} - {self.vin}'
+
+
+class Status(models.Model):
+    # 'Created', 'Canceled', or 'Finished'
+    # id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+    #    ordering = ("id",)  # Default ordering for Status
+        verbose_name_plural = "statuses"  # Fix the pluralization
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()    # auto_now=False, auto_now_add=False
+    reason = models.CharField(max_length=200)
+    status = models.ForeignKey(
+        Status,
+        related_name="+",  # do not create a related name on State
+        on_delete=models.PROTECT,
+    )
+    vin = models.CharField(max_length=17, unique=True)
+    customer = models.CharField(max_length=50)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return f'{self.reason} - {self.vin}'
