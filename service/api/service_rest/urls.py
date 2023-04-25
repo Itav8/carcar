f627b85aebf6bec293aed17e245883b6b8c489de
Jonathan <jonathan.r.blazer@gmail.com>
2023-04-25 11:19:23 -0400

    Tech/Appt views done except status update

---
+++ service/api/service_rest/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from .views import (
+    list_technicians,
+    delete_technician,
+    list_appointments,
+    delete_appointment,
+)
+
+urlpatterns = [
+    path(
+        "technicians/",
+        list_technicians,
+        name="list_technicians",
+    ),
+    path(
+        "technicians/<int:pk>/",
+        delete_technician,
+        name="delete_technician",
+    ),
+    path(
+        "appointments/",
+        list_appointments,
+        name="list_appointments",
+    ),
+    path(
+        "appointments/<int:pk>/",
+        delete_appointment,
+        name="delete_appointment",
+    ),
+]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,154 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
+from common.json import ModelEncoder
+from .models import Technician, AutomobileVO, Appointment, Status
 # Create your views here.
+
+
+class TechnicianListEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "employee_id",
+        "first_name",
+        "last_name",
+    ]
+
+
+class AppointmentListEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "date_time",
+        "id",
+        "reason",
+        "vin",
+        "customer",
+    ]
+
+    def get_extra_data(self, o):
+        return {"status": o.status.name,
+                "technician": f'{o.technician.first_name} {o.technician.last_name}',
+                }
+
+
+@require_http_methods(["GET", "POST"])
+def list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        data = []
+        for technician in technicians:
+            id = technician.id
+            employee_id = technician.employee_id
+            first_name = technician.first_name
+            last_name = technician.last_name
+            data.append(
+                {
+                    'id': id,
+                    'employee_id': employee_id,
+                    'first_name': first_name,
+                    'last_name': last_name,
+                }
+            )
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        Technician.objects.create(**content)
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianListEncoder,
+        )
+
+
+@require_http_methods(["DELETE"])
+def delete_technician(request, pk):
+    if request.method == "DELETE":
+        try:
+            tech = Technician.objects.get(id=pk)
+            tech.delete()
+            return JsonResponse(
+                tech,
+                encoder=TechnicianListEncoder,
+                safe=False,
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse({"message": "Technician does not exist"},
+                                status=400,
+                                )
+
+
+@require_http_methods(["GET", "POST"])
+def list_appointments(request):
+    if request.method == "GET":
+        appointments = Appointment.objects.all()
+        data = []
+        for appointment in appointments:
+            if appointments == []:
+                break
+            id = appointment.id
+            date = appointment.date_time.date()
+            time = appointment.date_time.time()
+            reason = appointment.reason
+            status = appointment.status
+            vin = appointment.vin
+            customer = appointment.customer
+            technician = appointment.technician.first_name
+            data.append(
+                {
+                    'id': id,
+                    'date': date,
+                    'time': time,
+                    'reason': reason,
+                    'status': status,
+                    'vin': vin,
+                    'customer': customer,
+                    'technician': technician,
+                }
+            )
+        return JsonResponse(
+            {"appointments": appointments},
+            encoder=AppointmentListEncoder,
+            safe=False,
+        )
+    else:       # POST - CREATE APPOINTMENT
+        content = json.loads(request.body)
+
+        try:
+            technician = Technician.objects.get(employee_id=content["technician"])
+            content["technician"] = technician
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid technician"},
+                status=400,
+            )
+        status = Status.objects.get(name="Created")
+        content["status"] = status
+        Appointment.objects.create(**content)
+        appointments = Appointment.objects.all()
+        return JsonResponse(
+            {"appointments": appointments},
+            encoder=AppointmentListEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["DELETE"])
+def delete_appointment(request, pk):
+    if request.method == "DELETE":
+        try:
+            app = Appointment.objects.get(id=pk)
+            app.delete()
+            return JsonResponse(
+                app,
+                encoder=AppointmentListEncoder,
+                safe=False,
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse({"message": "Appointment does not exist"},
+                                status=400,
+                                )
